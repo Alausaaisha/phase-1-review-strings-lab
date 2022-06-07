55e4077b7c001d2f1349a7c6d98159e570366721
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-07 09:57:20 +0100

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Tech sis Aisha';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
